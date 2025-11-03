db2cdc634c682d3724edd25dd786db8669be52be
youch <youch@25.19.215.229>
2025-11-03 21:10:44 +0900

    rsdsddasda

--- hi/src/hiqwe/qwer.java
+++ hi/src/hiqwe/qwer.java
@@ -3,6 +3,10 @@ package hiqwe;
 public class qwer {
 	public static void main(String[] args){
 		System.out.print("a");
+		System.out.print("a");
+		System.out.print("a");
+		System.out.print("a");
+		
 	}
 	
 	
